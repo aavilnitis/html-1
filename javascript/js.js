0d2e9d9b9cf0e336b908619fcf77d3410acc86ad
aavilnitis <aleksis.vilnitis@gmail.com>
2021-03-09 13:18:33 +0200

    Laikam ka pabeidzu?

--- javascript/js.js
+++ javascript/js.js
@@ -1,91 +1,107 @@
 var spelets = 0;
 var uzvara = document.getElementById("uzvara");
 function create(){
-    spelets++;
     let x = document.getElementById("izmers").value;
-    let table = document.getElementById("laukums");
-    var pari = x*x/2;
-    let skaitli = [];
-//    skaitli[0] = 101;
-   
-   //pirmā array 
-    for(t = 0; t < x*x/2; t++){
-        
-        var randsk = Math.floor(Math.random() * 50) + 1;
-        while(skaitli.includes(randsk)){
-            var randsk = Math.floor(Math.random() * 100) + 1;
+    if(((x*x/2 % 2)) != 0){
+        reload();
+        alert("Izvēlētā laukuma izmēra kvadrātam jādalās ar divi!");
+    }else{
+        spelets++;
+        let table = document.getElementById("laukums");
+        var pari = x*x/2;
+        let skaitli = [];
+
+    //    skaitli[0] = 101;
+
+       //pirmā array 
+        for(t = 0; t < x*x/2; t++){
+
+            var randsk = Math.floor(Math.random() * 50) + 1;
+            while(skaitli.includes(randsk)){
+                var randsk = Math.floor(Math.random() * 50) + 1;
+            }
+            skaitli[t] = randsk;
         }
-        skaitli[t] = randsk;
-    }
-    
-//katram pirmā array skaitlim izveidoju bināro skaitli un ieliku visus savā array
-    let biskaitli= [];
-    for (t = 0; t < (x*x)/2; t++){
-        biskaitli[t] = dec2bin(skaitli[t]);
-    }
-    
-    let koparray = [];
-    for(var i = 0; i < x*x/2; i ++){
-        koparray[i] = skaitli[i]
-    }
-    for(var i = 0; i<x*x/2; i++){
-        koparray[i+(x*x/2)] = biskaitli[i];
-    }
-    shuffleArray(koparray);
-   
-    
-    
-    for(let i =0; i< x; i++){
-        
-        let row = table.insertRow(i);        
-        for(let j =0; j< x; j++){
-            
-            let button = document.createElement('BUTTON');
-            button.classList.add("grid")
-            button.id = koparray[x*i+j];
-            
-            
-            var text = document.createTextNode(koparray[x*i+j]);
-            button.appendChild(text);
-            
-            let cell = row.insertCell(j);
-            cell.appendChild(button);
-            
-            var pirma;
-            var otra;
-            var pogasspiesana = 0;
-            
-            button.onclick=()=>{
-                
-               
-                
-                pogasspiesana++;
-                if(pogasspiesana == 1){
-//                  document.getElementById(pirma).style.backgroundColor = 'black';
-//                  document.getElementById(otra).style.backgroundColor = 'black';
-                    pirma = button.id;
-//                    var tests = 356;
-//                    alert(tests.toString().length);
-                    //alert(pirma);
-                }else if(pogasspiesana == 2){
-                    otra = button.id;  
-                    //alert(otra);
-                    salidzinasana(pirma,otra);
-                    pogasspiesana =0;
-                    if(uzminetie == pari){
-                        var laiks = time.toString();
-                        window.location.href = "uzvara.html";
-                        alert("Jūsu laiks ir: " +laiks);
+
+    //katram pirmā array skaitlim izveidoju bināro skaitli un ieliku visus savā array
+        let biskaitli= [];
+        for (t = 0; t < (x*x)/2; t++){
+            biskaitli[t] = dec2bin(skaitli[t]);
+        }
+
+        let koparray = [];
+        for(var i = 0; i < x*x/2; i ++){
+            koparray[i] = skaitli[i]
+        }
+        for(var i = 0; i<x*x/2; i++){
+            koparray[i+(x*x/2)] = biskaitli[i];
+        }
+        shuffleArray(koparray);
+
+
+
+        for(let i =0; i< x; i++){
+
+            let row = table.insertRow(i);        
+            for(let j =0; j< x; j++){
+
+                let button = document.createElement('BUTTON');
+                button.classList.add("grid");
+
+                //document.getElementsByClassName('grid').style.color = 'black';
+                button.id = koparray[x*i+j];
+
+
+                var text = document.createTextNode(koparray[x*i+j]);
+                button.appendChild(text);
+
+                let cell = row.insertCell(j);
+                cell.appendChild(button);
+
+                var pirma;
+                var otra;
+                var pogasspiesana = 0;
+
+                button.onclick=()=>{
+
+                    if(button.style.backgroundColor == 'green'){
+                        
+                    }else{
+                    
+                    button.style.color = "white";
+                    setTimeout(function(){
+                        button.style.color = "black";    
+                    }, 3000)
+                    
+                    pogasspiesana++;
+                    if(pogasspiesana == 1){
+    //                  document.getElementById(pirma).style.backgroundColor = 'black';
+    //                  document.getElementById(otra).style.backgroundColor = 'black';
+                        pirma = button.id;
+                    }else if(pogasspiesana == 2){
+                        otra = button.id;  
+                        //alert(otra);
+                        salidzinasana(pirma,otra);
+                        pogasspiesana =0;
+                        
+                        if(uzminetie == pari){
+                            setTimeout(function(){
+                                var laiks = time.toString();
+                                window.location.href = "uzvara.html";
+                                alert("Jūsu laiks ir: " +laiks); 
+                            }, 1500)
+                        }
+                    }
+
+    //                else if(pogasspiesana >2){
+    //                    pogasspiesana =0;
+    //                    //button.parentNode.parentNode.parentNode.remove();
+    //                }
+
                     }
                 }
-                
-//                else if(pogasspiesana >2){
-//                    pogasspiesana =0;
-//                    //button.parentNode.parentNode.parentNode.remove();
-//                }
-                
-            }
-        }   
+            }   
+        }
     }
 }
 function salidzinasana(pirma,otra){
